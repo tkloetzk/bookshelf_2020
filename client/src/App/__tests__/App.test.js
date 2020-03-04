5e82b15d2692542962ad44830eceeb926015304e
Tucker Kloetzke <tuckerkloetzke@gmail.com>
2020-03-04 08:08:30 -0600

    App tests

--- client/src/App/__tests__/App.test.js
+++ client/src/App/__tests__/App.test.js
@@ -1,8 +1,28 @@
 import '@testing-library/jest-dom'
 import React from 'react'
-import {render, fireEvent, screen} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import App from '../App'
 
 test('renders correctly and changes tabs', () => {
-    expect(true).toBe(true)
+    const { queryByText, queryAllByRole, debug } = render(<App />)
+    const header = queryByText('Book Review Aggregator')
+    let [searchTab, bookshelfTab] = queryAllByRole('tab')
+    const searchTabContent = queryByText('0')
+    const bookshelfTabContent = queryByText('1')
+
+    expect(header).not.toBeNull()
+
+    expect(searchTab).not.toBeNull()
+    expect(searchTab).toHaveAttribute('aria-selected', 'true')
+    expect(searchTabContent).not.toBeNull()
+
+    expect(bookshelfTab).not.toBeNull()
+    expect(bookshelfTab).toHaveAttribute('aria-selected', 'false')
+    expect(bookshelfTabContent).toBeNull()
+
+    debug(bookshelfTab)
+    fireEvent.click(bookshelfTab)
+    [searchTab, bookshelfTab] = queryAllByRole('tab')
+    expect(searchTab).toHaveAttribute('aria-selected', 'true')
+
 })
